70708e53371ba464df869f51b4e46b22ec330444
David Pérez Conde <dpconde.me@gmail.com>
2022-10-17 15:36:51 +0200

    implement databinding for login

--- feature/login/src/main/java/com/dpconde/kaicare/feature/login/presentation/LoginFragment.kt
+++ feature/login/src/main/java/com/dpconde/kaicare/feature/login/presentation/LoginFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dpconde.kaicare.feature.login.R
+import com.dpconde.kaicare.feature.login.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
+
     }
 }
