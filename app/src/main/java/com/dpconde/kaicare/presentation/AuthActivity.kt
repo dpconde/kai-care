e6b4b095a44be42603a9baad628bb114078a8174
David Pérez Conde <dpconde.me@gmail.com>
2022-10-17 11:16:15 +0200

    make login full screen

--- app/src/main/java/com/dpconde/kaicare/presentation/AuthActivity.kt
+++ app/src/main/java/com/dpconde/kaicare/presentation/AuthActivity.kt
@@ -2,11 +2,13 @@ package com.dpconde.kaicare.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.dpconde.kaicare.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_auth)
     }
 }
