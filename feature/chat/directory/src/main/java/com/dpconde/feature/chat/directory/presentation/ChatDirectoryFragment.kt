b19a1ac84b873fa92ea7bfa2359b9e975abf2e39
David Pérez Conde <dpconde.me@gmail.com>
2022-10-20 20:08:43 +0200

    add databinding in fragment

--- feature/chat/directory/src/main/java/com/dpconde/feature/chat/directory/presentation/ChatDirectoryFragment.kt
+++ feature/chat/directory/src/main/java/com/dpconde/feature/chat/directory/presentation/ChatDirectoryFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dpconde.feature.chat.directory.R
+import com.dpconde.feature.chat.directory.databinding.FragmentChatDirectoryBinding
 
 
 class ChatDirectoryFragment : Fragment() {
 
+    private lateinit var binding: FragmentChatDirectoryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_chat_directory, container, false)
+    ): View {
+
+        binding = FragmentChatDirectoryBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
     }
 }
