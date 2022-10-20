ee1f6e093012f7733b045f196816f7b1abf93433
David Pérez Conde <dpconde.me@gmail.com>
2022-10-20 18:53:03 +0200

    add main activity + navigation + menu

---
+++ app/src/main/java/com/dpconde/kaicare/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dpconde.kaicare.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dpconde.kaicare.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
