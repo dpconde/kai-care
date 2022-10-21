f8a1aabd6c3c5b48ea27eaae41ed306d7b96000a
David Pérez Conde <dpconde.me@gmail.com>
2022-10-21 15:37:19 +0200

    start mainActivity from authActivity

--- app/src/main/java/com/dpconde/kaicare/presentation/AuthActivity.kt
+++ app/src/main/java/com/dpconde/kaicare/presentation/AuthActivity.kt
@@ -1,7 +1,8 @@
 package com.dpconde.kaicare.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.dpconde.kaicare.R
 
@@ -10,5 +11,11 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_auth)
+    }
+
+    fun goToMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
