f0e04958c1d7e8072d8676e8efe0dbe48489bc75
David Pérez Conde <dpconde.me@gmail.com>
2022-10-23 12:20:14 +0200

    create dummy menu fragments

--- app/src/main/java/com/dpconde/kaicare/presentation/MainActivity.kt
+++ app/src/main/java/com/dpconde/kaicare/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.dpconde.kaicare.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.dpconde.kaicare.R
+import com.dpconde.kaicare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        this.setupNavController()
+    }
+
+    private fun setupNavController(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
